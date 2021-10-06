2f2f89e1ee6b2e070666b0d3b0a31ae97c841acb
Terqaz <holinvova@gmail.com>
2021-10-06 23:48:57 +0300

    - Сделал отправление файла в вк

--- src/test/java/com/my/MainTest.java
+++ src/test/java/com/my/MainTest.java
@@ -1,33 +1,42 @@
 package com.my;
 
-import com.my.models.Group;
-import com.my.services.lstu.LstuParser;
 import org.junit.jupiter.api.Test;
 
-import java.util.Map;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 class MainTest {
 
-//    @Test
-//    void reactorTrying () {
-//        Flux
-//                .generate(() -> )
-//                .interval(Duration.of(1, ChronoUnit.DAYS))
-//    }
-
     @Test
-    void prodAddNewDataInGroup () {
-        final LstuParser lstuParser = LstuParser.getInstance();
-        final GroupsRepository groupsRepository = GroupsRepository.getInstance();
+    void completableFutures_using () {
+        final List<CompletableFuture<Void>> futures = Stream.generate(
+                () -> CompletableFuture.runAsync(() -> {
+                        try {
+                            delayedPrint();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                }))
+                .limit(50)
+                .collect(Collectors.toList());
 
-        for (Group group : groupsRepository.findAll()) {
-            final Map<String, String> lkIds = lstuParser.getSubjectsGeneralLkIds("2021-О");
-            group.setLkIds(
-                    lkIds.get(LstuParser.SEMESTER_ID),
-                    lkIds.get(LstuParser.GROUP_ID),
-                    lkIds.get(LstuParser.CONTINGENT_ID)
-            );
+        final CompletableFuture<Void> allOf = CompletableFuture.allOf(
+                futures.toArray(new CompletableFuture[futures.size()]));
+
+        try {
+            allOf.get();
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
         }
     }
 
+    static int i = 1;
+
+    void delayedPrint () throws InterruptedException {
+        System.out.println(i);
+        i++;
+    }
 }
--- src/test/java/com/my/services/VkBotServiceTest.java
+++ src/test/java/com/my/services/VkBotServiceTest.java
@@ -3,15 +3,25 @@ package com.my.services;
 import com.my.services.vk.VkBotService;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-
+// Переменные окружения VK_GROUP_TOKEN
 class VkBotServiceTest {
 
     VkBotService vkBot = VkBotService.getInstance();
+    static final int APP_ADMIN_ID = 173315241;
 
     @Test
     void getUserNameIsCorrect () {
         assertEquals("Павел Дуров", vkBot.getUserName(1));
+    }
+
+    @Test
+    void fileUpload () {
+        vkBot.sendMessageTo(APP_ADMIN_ID,
+                new File("C:\\Users\\Terqaz\\Desktop\\LKBot\\src\\main\\java\\com\\my\\Main.java"),
+                "Тестовый файл");
     }
 }
